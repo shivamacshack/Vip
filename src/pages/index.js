5fdc691c1a034654c2ac0e7a07f5f1618bdabc6f
Hung Do <hung.dothixuan@gmail.com>
2020-04-11 10:41:25 +1000

    mute the video and audio of every participant whose name contains 'secondary_guest'

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,26 +13,39 @@ const Video = ({ token }) => {
     TwilioVideo.connect(token, { video: true, audio: true, name: "test" }).then(
       room => {
         // Attach the local video
-        TwilioVideo.createLocalVideoTrack().then(track => {
-          localVidRef.current.appendChild(track.attach())
-        })
+        console.log(token)
+        var jwt = require('jsonwebtoken');
+        var decoded = jwt.decode(token);
+        var identity = decoded.grants.identity;
+        console.log(identity);
+        console.log("local name: ", identity)
+        console.log("identity.includes('secondary_guest') = ", identity.includes('secondary_guest'))
+        if (!identity.includes('secondary_guest')){
+            TwilioVideo.createLocalVideoTrack().then(track => {
+              localVidRef.current.appendChild(track.attach())
+            })
+        }
 
         const addParticipant = participant => {
-          console.log("new participant!")
-          console.log(participant)
-          participant.tracks.forEach(publication => {
-            if (publication.isSubscribed) {
-              const track = publication.track
+          console.log("new remote participant!")
+          console.log("remote name: ", participant.identity)
+          console.log("participant.identity.includes('secondary_guest') = ", participant.identity.includes('secondary_guest'))
+          if (participant.identity.includes('secondary_guest')){console.log("shouldn't show video")}
+          if (!participant.identity.includes('secondary_guest')){console.log("this is not a secondary guest")
+              participant.tracks.forEach(publication => {
+                if (publication.isSubscribed) {
+                  const track = publication.track
 
-              remoteVidRef.current.appendChild(track.attach())
-              console.log("attached to remote video")
+                  remoteVidRef.current.appendChild(track.attach())
+                  console.log("attached to remote video")
+                }
+              })
+
+              participant.on("trackSubscribed", track => {
+                console.log("track subscribed")
+                remoteVidRef.current.appendChild(track.attach())
+              })
             }
-          })
-
-          participant.on("trackSubscribed", track => {
-            console.log("track subscribed")
-            remoteVidRef.current.appendChild(track.attach())
-          })
         }
 
         room.participants.forEach(addParticipant)
@@ -43,8 +56,8 @@ const Video = ({ token }) => {
 
   return (
     <div>
-      <div ref={localVidRef} />
-      <div ref={remoteVidRef} />
+      <p>Local video: </p><div ref={localVidRef} />
+      <p>Remote video: </p><div ref={remoteVidRef} />
     </div>
   )
 }
